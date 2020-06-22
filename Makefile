3281376c136b89a9e24b0d6ef616b952f175e6c1
Devin Dollery <ddevin@atmos.ucla.edu>
2020-06-22 16:37:39 -0700

    Non-analytical WEC model working.
    The old code version for WEC non-analytical was quite different from the analytical version worked from in previous commits. Therefore I had to change wec_forces substanially to account for different WEC equations in non-analytical old code vs analytical old code. This impacted read in variables and thus changed other sections of code, too. Further work had been done on the analytical example and that code can be found on the ROMS onedrive folder.
    
    prsgrd32AC1.F:
    - Addition of WEC needed to be in seperate loop. This was bug that caused MPI issue (funny values at MPI tile boundaries).
    
    pre_step3d4S.F:
    - All terms from rhs3d35S.F in the old code had only been added to step3d_uv1.F and not pre_step3d4S.F, so I added thes terms from rhs3d.
    
    Makefile.F:
    - Moved main.F to end of build list because it now calls a WEC module subroutine and thus the module needs to compile before main.F.
    
    wec_frc.F:
    - wec_forces and wec_forces_tile were created to replicate mrl_wci and mrl_wci_tile. The method using private scratch arrays was used as per old code (hadn't done this previously).
    - Added read_wec_averages. Not entirely sure this is working correctly though, didn't check.
    - Added all wec variables from forces.h of non-analytical code (new variables like ustr0 which weren't in analytical version of code).
    - This goes for terms in ncvars.h too.
    - It includes my attempted pointer method for assigning netcdf variables (it is commented out though as I couldn't get it to work when writing a second netcdf file, only worked for one file). Had to revert back to old code method (longer code).
    - Fixed bug in old code's analytical forcing where x does not equal zero at node 1 which it is suppossed to equal.
    - Fixed confusion in old code of ust2d and zeta averaging. Both were treated as if on same points.

--- Makefile
+++ Makefile
@@ -19,7 +19,7 @@ include Makedefs/Makedefs.IntelEM64T
 #   3) boundary conditions;        7) netCDF I/O routines;
 #   4) sea-water EOS routines;     8) model forcing routines;
 
-KRNSRC = main.F		dimensions.F	river_frc.F	wec_frc.F	step2d_FB.F	read_inp.F\
+KRNSRC = dimensions.F	river_frc.F	wec_frc.F	step2d_FB.F	read_inp.F\
 	set_weights.F	set_scoord.F	init_scalars.F	init_arrays.F\
 	ana_init.F	setup_grid1.F	setup_grid2.F	visc_rescale.F\
 	set_nudgcof.F	ana_grid.F	analytical.F\
@@ -39,7 +39,7 @@ KRNSRC = main.F		dimensions.F	river_frc.F	wec_frc.F	step2d_FB.F	read_inp.F\
         lenstr.F	setup_kwds.F	check_srcs.F	check_switches1.F\
         get_date.F	ext_copy_prv2shr.F		check_switches2.F\
 \
-	mpi_setup.F	mpi_exchange8TA.F
+	mpi_setup.F	mpi_exchange8TA.F	main.F		
 		  
 
 
--- cppdefs.h
+++ cppdefs.h
@@ -707,36 +707,120 @@ c---# define AVERAGES
 
 /* WEC */
 
-# define ANA_WEC_FRC
+# undef ANA_WEC_FRC /* DEFINE FOR ANALYTICAL WEC */
 
-/* DevinD - from DH's wave_packet cppdefs.h */
+/* DevinD - from DH's analytical wave_packet cppdefs.h */
 
-# undef VERBOSE
-# define ANA_GRID
-# define ANA_INITIAL
-# define ANA_SMFLUX
-# define ANA_SRFLUX
-# define ANA_STFLUX
-# undef ANA_SST
+# ifdef ANA_WEC_FRC
+#  undef VERBOSE
+#  define ANA_GRID
+#  define ANA_INITIAL
+#  define ANA_SMFLUX
+#  define ANA_SRFLUX
+#  define ANA_STFLUX
+#  undef ANA_SST
 
-# undef SALINITY
-# define ANA_SSFLUX
-# define SOLVE3D
-# define UV_ADV
-# define NONLIN_EOS
-# define SPLIT_EOS
+#  undef SALINITY
+#  define ANA_SSFLUX
+#  define SOLVE3D
+#  define UV_ADV
+#  define NONLIN_EOS
+#  define SPLIT_EOS
 
-# define EW_PERIODIC
-# define NS_PERIODIC
+#  define EW_PERIODIC
+#  define NS_PERIODIC
 
-# define MRL_WCI
-# ifdef MRL_WCI
-#  define ANA_WWAVE
-#  define BRK0
+#  define MRL_WCI
+#  ifdef ANA_WEC_FRC
+#   define ANA_WWAVE
+#  endif
+#  ifdef WEC
+#   define ANA_WWAVE
+#   define BRK0
+#  endif
+
+# endif /* ANA_WEC_FRC */
+
+/* DevinD - end from DH's analytical wave_packet */
+
+/* DevinD - start DH's non-analytical WEC */
+
+# define USWC_WEC /* DEFINE FOR NON-ANALYTICAL WEC - US West Coast with WEC */
+
+/* -------------------------------------------------------  */
+
+# if defined USWC_WEC
+
+#  undef GRID_ANG_DEG
+#  define SOLVE3D
+#  define UV_COR
+#  define UV_ADV
+
+#  define CURVGRID
+#  define SPHERICAL
+#  define MASKING
+
+#  define SALINITY
+#  define NONLIN_EOS
+#  define SPLIT_EOS
+
+#  define AVERAGES /* DevinD uncommented */
+c# define EXACT_RESTART
+#  define NEW_S_COORD
+c# define IMPLICIT_BOTTOM_DRAG
+
+#  define UV_VIS2
+#  define MIX_GP_UV
+#  define TS_DIF2
+#  define MIX_GP_TS
+
+#  define LMD_MIXING
+#  define LMD_KPP
+#  define LMD_BKPP
+#  define LMD_CONVEC
+#  define LMD_NONLOCAL
+
+#  define OBC_WEST
+#  define OBC_EAST
+#  define OBC_NORTH
+#  define OBC_SOUTH
+
+#  define OBC_M2FLATHER
+#  define OBC_M3ORLANSKI
+#  define OBC_TORLANSKI
+
+#  define Z_FRC_BRY
+#  define M2_FRC_BRY
+#  define M3_FRC_BRY
+#  define T_FRC_BRY
+#  define SPONGE
+
+#  define MRL_WCI
+#  ifdef WEC
+#    define BRK0
+#    define SURFACE_BREAK
+#    undef SPEC_DD
+#    undef LOG_BDRAG
+#    undef WKB_WWAVE
+#    undef BBL
+#    define WAVE_OFFLINE
+#    define WAVE_FRICTION
+#    define BBL_S95
+c#    define BBL_F00
+#    define SUP_OFF
+#    define WAVE_DIFF_FROM_LM
+#  else
+#   define LOG_BDRAG
+#  endif
+#  undef BULK_FLUX
+#  ifdef BULK_FLUX
+#   define COUPLED_SURF_CURR
+#   define WND_AT_RHO_POINTS
+#  endif
+
 # endif
 
-
-/* DevinD - end from DH's wave_packet cppdefs.h */
+/* DevinD - end DH's non-analytical WEC */
 
 #endif
 
