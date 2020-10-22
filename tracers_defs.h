7807f51a217ea4121125e1e893d2676910799bde
Devin Dollery <ddevin@atmos.ucla.edu>
2020-10-22 07:18:56 -0700

    Passive tracer module: set_ana_tracer_flux
    - Setting passive tracers extended to support analytical surface tracer flux.
    
    Tracers.F
    - Added: set_ana_surf_tracer_flx
    - Test with stflx=0., stflx=0.5, and checked error message working.
    - Moved 't' tracer variable from ocean3d.h to the tracer module
    - Did not move 'NT' variable as it is referenced through ncvars.h in so many places. Save that for another time or move it to dimensions.F module in future.

--- tracers_defs.h
+++ tracers_defs.h
@@ -16,24 +16,28 @@
 !  defined at compile time, and is not stored in an array like the
 !  the rest of the variables that belong to each tracer.)
 
-! itrace1 = integer to identify tracer in tracer array 't'
-! wrt_t   = True/False whether to print tracer to output file
-! t_vname = Tracer short name
-! t_units = Tracer units (for outputing tracer)
-! t_lname = Tracer long name (for outputing tracer)
+! itrace1   = integer to identify tracer in tracer array 't'
+! wrt_t     = True/False whether to print tracer to output file
+! t_vname   = Tracer short name
+! t_units   = Tracer units (for outputing tracer)
+! t_lname   = Tracer long name (for outputing tracer)
+! t_ana_frc = Whether surf flux is read in (0), or analytical (1)
+!             Could be extended 2,3,... depending on different types of
+!             analytical forcing, e.g. time variant/invariant, and further
+!             specific for each variable using its 'itrace' index...
 
 
-      itrace1=1+iTandS;              wrt_t(itrace1) =.True.;
-      t_vname(itrace1)='trace1';     t_units(itrace1)='%/%/%';
-      t_tname(itrace1)='trace1_time'
+      itrace1=1+iTandS;              wrt_t(itrace1) =.True.
+      t_vname(itrace1)='trace1';     t_units(itrace1)='%/%/%'
+      t_tname(itrace1)='trace1_time';t_ana_frc(itrace1)=0
       t_lname(itrace1)='long trace1'
 
       isalt2=2+iTandS;               wrt_t(isalt2) =.True.
       t_vname(isalt2)='salt2';       t_units(isalt2)='PSUuu'
-      t_tname(isalt2)='salt2_time'
+      t_tname(isalt2)='salt2_time';  t_ana_frc(isalt2)=0
       t_lname(isalt2)='long salt2'
 
-!      iptrace2=2+iTandS;            wrt_t(iptrace2) =.True.
-!      t_vname(iptrace2)='ptrace2';  t_units(iptrace2)='uuunits'
-!      t_tname(iptrace2)='ptrace2_time'
+!      iptrace2=2+iTandS;             wrt_t(iptrace2) =.True.
+!      t_vname(iptrace2)='ptrace2';   t_units(iptrace2)='uuunits'
+!      t_tname(iptrace2)='ptrace2_time';t_ana_frc(iptrace2)=0
 !      t_lname(iptrace2)='long ptrace2'
