f60ec624e28986b6ab6ca376fc5bd214370216db
Devin Dollery <ddevin@atmos.ucla.edu>
2021-01-14 12:10:32 -0800

    code check: added netcdf output test
    Examples/code_check:
    - addde netcdf output check.
    - modified test_roms.sh script to be a loop, so easier to extend for additional test examples.
    
    read_write.F:
    - ncdf_read_coarse_grid - Daniel told me that this didn't compile if using NS_PERIODIC & EW_PERIODIC so I added cpp flags around it, as it is typically not needed with those flags anyway.

--- Examples/code_check/test_roms.py
+++ Examples/code_check/test_roms.py
@@ -3,6 +3,8 @@
 # -> compares the diagnostic numbers to ensure they haven't changed.
 # -> only useful when you expect result to be exactly the same.
 #
+# Add the appropriate .log file in the USER INPUTS section.
+#
 # Old roms code did a similar test using 'etalon' as can still be seen
 # in diag.F.
 
@@ -32,7 +34,7 @@ def search_string_in_file(file_name, string_to_search):
 
 # -----------------------------------------------------------------
 
-# ******* start user inputs ************
+# ******* USER INPUTS START ************
 # Tests & file names of roms terminal output logs
 test_names = ['WEC_real','Rivers_ana'] # ,'Rivers_ana'
 ntests = np.size(test_names)
@@ -42,13 +44,18 @@ filename_res = os.path.join(os.getcwd(),'benchmarks/wec/sample_wec_test.log')
 filenames = [ filename_BM, filename_res ]
 #print(filenames)
 # -- Rivers_ana
-filename_res = os.path.join(os.getcwd(),'benchmarks/river/river_ana_test.log')
 filename_BM  = os.path.join(os.getcwd(),'benchmarks/river/river_ana_bm.log')
+filename_res = os.path.join(os.getcwd(),'benchmarks/river/river_ana_test.log')
 filenames = np.append(filenames,[ filename_BM, filename_res ])
 filenames = np.reshape(filenames,(ntests,2))
 #print(np.shape(filenames))
-# ******* end user inputs **************
+# -- netcdf test of his, avg & rst
+fname_ncdf_BM  = os.path.join(os.getcwd(),'benchmarks/netcdf/wec_netcdf_bm.log')
+fname_ncdf_res = os.path.join(os.getcwd(),'benchmarks/netcdf/wec_netcdf_test.log')
+fnames_ncdf = [ fname_ncdf_BM, fname_ncdf_res ]
+# ******* USER INPUTS END **************
 
+# --------------------------------------------
 # Diagnostic terms
 fields = ['KINETIC_ENRG','BAROTR_KE','MAX_ADV_CFL','MAX_VERT_CFL']
 diags = np.zeros((ntests,2,4)) # 2 for bm vs new, 4 for fields
@@ -123,14 +130,60 @@ for test in range(ntests):
 		print(text)
 		file_diag.write(text)
 
+# ----------------------------------------------------------
+# Confirm netcdf functionality working for 3 types:
+# his (history) , avg (averages), and rst (restart) files
+#
+# Crudely checks that run has outputted the desired number
+# of steps. Test that results are correct is covered in
+# tests above. Didn't include the netcdf output above because
+# the printing of 'wrt_his: ...' messes up the loop of counting
+# the diagnostic values.
 
+# netcdf terms
+types = ['history','restart','averages']
+outputs   = np.zeros((2,3)) # 2 for bm vs new, 3 for types
+out_diffs = np.zeros((3)) # Differences between bm vs new fiels - 3 for types
+
+text='Checking netcdf: outputting of history, restart & averages'
+print(text)
+file_diag.write('\n'+text+'\n')
+
+# Loop through BM values and new values
+for m in range(2):
+
+	# read number of timesteps
+	line_num = search_string_in_file(fnames_ncdf[m],'Records written:')
+	line = open(fnames_ncdf[m], 'r').readlines()[line_num[0]-1] # -1 for 0 counting
+	#print('line= ' + line)
+	outputs[m,0] = int(line[42:44]) # Number of history steps written
+	#print('history= '  + str(outputs[m,0]))
+	outputs[m,1] = int(line[56:58]) # Number of restart steps written
+	#print('restart= '  + str(outputs[m,1]))
+	outputs[m,2] = int(line[71:73]) # Number of history steps written
+	#print('averages= ' + str(outputs[m,2]))
+
+out_diffs = outputs[0,:]-outputs[1,:]
+
+# Confirm outcome to user:
+if np.all(out_diffs==0.0):
+	text='Netcdf output works! --> numbers of history, restart & averages records match the benchmark.\n'
+	print(text)
+	file_diag.write(text)
+else:
+	text='ERROR! --> Netcdf output not working.\n'
+	print(text)
+	file_diag.write(text)	
+	
+	
+# ----------------------------------------------------------
 # Confirm overall outcome to user:
-if np.all(diffs==0.0):
-    text='\nCODE CORRECT! --> All diagnostics match the benchmarks. \nSee code_check.log file for details.'
+if np.all(diffs==0.0) and np.all(out_diffs==0.0):
+    text='\nCODE CORRECT! --> All diagnostics & netcdf outputting match the benchmarks. \nSee code_check.log file for details.'
     print(text)
     file_diag.write(text)
 else:
-    text='\nERROR! --> Tests do not match benchmark results. \nSee code_check.log file for details.'
+    text='\nERROR! --> Tests do not match benchmark results. \nEnsure using correct benchmark for machine. Deafult machine is Maya. \nSee code_check.log file for details.'
     print(text)
     file_diag.write(text)
 
