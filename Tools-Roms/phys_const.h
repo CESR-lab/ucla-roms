f0dbac4c49b570001717b9f3102d0598d59483c0
Devin Dollery <ddevin@atmos.ucla.edu>
2021-08-03 12:16:40 -0700

    Added zslice & fixed time dimension
    ncjoin_mpi creates joined file with a fixed time dimension.
    The old zslice needed an unlimited time dimension.
    Edited zslice to allow for fixed or unlimited time dimension.
    Added zslice & fixed time dimension
    
    ncjoin_mpi creates joined file with a fixed time dimension.
    The old zslice needed an unlimited time dimension.
    Edited zslice to allow for fixed or unlimited time dimension.

---
+++ Tools-Roms/phys_const.h
@@ -0,0 +1,4 @@
+      real(kind=8), parameter  ::  pi=3.14159265358979323846D0,
+     &     Eradius=6371315.D0, SiderealDay=23.9344699D0*3600.D0,
+     &                   day2sec=86400.D0, sec2day=1.D0/day2sec,
+     &                     deg2rad=pi/180.D0, rad2deg=180.D0/pi
