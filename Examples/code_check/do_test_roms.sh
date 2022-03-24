0add74bdd5b23b4c1bd62fd5d78893223fb33b65
Devin Dollery <ddevin@atmos.ucla.edu>
2022-03-24 16:16:52 -0700

    code_check script works for maya and expanse
    Added log files for maya and expanse (since they give different solutions (different compiler versions))
    
    cd Examples/code_check
    ./do_test_all.sh maya (or expanse)
    
    Takes argument depending on machine (maya or expanse).

--- Examples/code_check/do_test_roms.sh
+++ Examples/code_check/do_test_roms.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# need this here as well, in case example run on its own:
+if [ "$1" != "expanse" -a "$1" != "maya" -a "$1" != "laptop" ]
+then
+echo "Script must have argument 'expanse' or 'maya'! E.g.: './do_test_all.sh maya'. Try again!"
+exit
+fi
+
+bm_file="benchmark.result_$1"                    # set benchmark specific to machine (maya/expanse)
+#echo "$bm_file"
+
 # 1) Run test case:
 #echo "  test compiling..."  
 cp ../../code_check/diag.F ../                   # need special diagnostic output for full precision
@@ -7,11 +17,11 @@ cd ../
 make &> /dev/null
 echo "  test running..."
 mpirun -n 6 ./roms code_check/benchmark.in > code_check/test.log
-rm diag.F roms test_his.*.*.nc
+rm diag.F roms test_his.*.*.nc grid.*.nc &> /dev/null  # grid needed for analytical examples
 cd code_check
 
 # 2) Python - confirm values:
-python3 test_roms.py
+python3 test_roms.py $bm_file
 retval=$?
 #echo $retval
 
