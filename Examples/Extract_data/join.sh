2bef12db4a6b5c4cbdb93b00bb7589387b24b9af
Devin Dollery <ddevin@atmos.ucla.edu>
2022-04-26 17:11:52 -0700

    extract_data: readme update
    Fixed documentation for extract_data to replace readmes for bry_child_extract

--- Examples/Extract_data/join.sh
+++ Examples/Extract_data/join.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 ncjoin -d sample_his*.?.nc
-ncjoin -d sample_dia*.?.nc
-join_child_bry -d sample_child_bry.0000.?.nc
-join_child_bry -d sample_child_bry_2.0000.?.nc
-join_child_bry -d sample_child_bry.0002.?.nc
-join_child_bry -d sample_child_bry_2.0002.?.nc
-ncview sample_child_bry.0000.nc &
+extract_data_join sample_ext.00000.?.nc
+ncview grid1.00000.nc &
