007bba966ef3990a238d293e8650639ad5fce3cb
Devin Dollery <ddevin@atmos.ucla.edu>
2022-05-24 16:00:26 -0700

    Code checking to include -fp-model strict
    This helps with numerical consitency, so that if we shuffle things around without trying to change
    the answer, the answer is less likely to change...

--- Examples/code_check/do_test_roms.sh
+++ Examples/code_check/do_test_roms.sh
@@ -11,11 +11,12 @@ bm_file="benchmark.result_$1"                    # set benchmark specific to mac
 #echo "$bm_file"
 
 # 1) Compile test case:
-echo "  test compiling..."  
+echo "  test compiling..."                       # delete any roms executable that might be there
 cd ../
-rm roms &> /dev/null                             # delete any roms executable that might be there
-cp ../code_check/diag.F .                        # need special diagnostic output for full precision
-mkdir opt_tmp                                    # store opt files of example so not over-written
+rm roms &> /dev/null                             # need special diagnostic output for full precision
+cp -p ../code_check/diag.F .                     # store opt files of example so not over-written
+cp -p ../code_check/Makedefs.inc .               # Need special Makedefs to ensure consistency in result
+mkdir opt_tmp                                    # using -fp-model strict
 mv *.opt opt_tmp
 cp -p opt_tmp/cppdefs.opt .
 cp -p opt_tmp/param.opt .
@@ -32,7 +33,7 @@ mpirun -n 6 ./roms code_check/benchmark.in > code_check/test.log
 fi
 
 rm diag.F roms test_his.*.*.nc grid.*.nc &> /dev/null  # grid needed for analytical examples
-rm *.opt                                               # copy original example opt files back
+rm *.opt Makedefs.inc                                  # copy original example opt files back
 cp -p opt_tmp/*.opt .
 rm -r opt_tmp
 cd code_check
