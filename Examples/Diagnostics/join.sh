1e894c0f1191fc0c9713b71ab7de974ab85b488e
Jeroen Molemaker <nmolem@atmos.ucla.edu>
2023-05-11 11:50:45 -0700

    Fixed tracers diagnostics

--- Examples/Diagnostics/join.sh
+++ Examples/Diagnostics/join.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-ncjoin -d sample_his*.?.nc
-ncjoin -d sample_dia*.?.nc
-#ncjoin -d sample_spn*.?.nc
-#ncjoin -d sample_rst*.?.nc
-#ncjoin -d sample_stn*.?.nc
-ncview sample_dia.*.nc
+ncjoin -d diags_his*.?.nc
+ncjoin -d diags_dia*.?.nc
+ncview diags_dia.*.nc
