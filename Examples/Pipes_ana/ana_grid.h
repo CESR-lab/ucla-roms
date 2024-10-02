d5d7528ee2acf3f725de85d360feacef397a5c31
Garrett <garrett.staller@gmail.com>
2024-10-02 16:33:31 -0700

    New branch working to streamline initialization

--- Examples/Pipes_ana/ana_grid.h
+++ Examples/Pipes_ana/ana_grid.h
@@ -11,7 +11,6 @@
       real dh, shelf, slope, land, coast
       real :: depth,max_depth
       real riv_west, riv_east,riv_cells
-      real psz,px,py,pipe_cells
 
       SizeX = 30.0e3  !! Domain size in x-direction [m]
       SizeY = 30.0e3  !! Domain size in y-direction [m]
@@ -94,21 +93,3 @@
         enddo
       enddo
 
-      if (pipe_source) then
-        psz = sizeX*0.02 ! Width of the pipe
-        px  = sizeX*.5  ! x location pipe 
-        py  = sizeY*.5  ! y location pipe 
-        pipe_cells = nint(psz/dx)**2 !number of cells in this pipe
-        do j=-1,ny+2
-          do i=-1,nx+2
-            pipe_fraction(i,j) = 0.0
-            pipe_idx(i,j) = 0
-            if (xr(i,j)> px-0.5*psz .and. xr(i,j)<px+0.5*psz) then
-              if (yr(i,j)> py-0.5*psz .and. yr(i,j)<py+0.5*psz) then
-                 pipe_fraction(i,j) = 1.0/pipe_cells
-                 pipe_idx(i,j) = 1
-              endif
-            endif
-          enddo
-        enddo
-      endif
