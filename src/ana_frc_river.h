203e760360256291e1e8b5e38a28a144fecf5982
Jeroen Molemaker <nmolem@atmos.ucla.edu>
2025-05-14 12:50:45 -0700

    Checkout of new ana_frc_river.h from ifx_funcionality

--- src/ana_frc_river.h
+++ src/ana_frc_river.h
@@ -7,6 +7,4 @@
       ! Set river volume and tracer values for each time step
       riv_vol(1)   = 5e2  ! Volume flux in m3/s
       riv_trc(1,1) = 24.0 ! Temperature in  Degrees C
-      riv_trc(1,2) =  1.0  ! Salinity in PSU
-      
-      
+!     riv_trc(1,2) =  1.0  ! Salinity in PSU
