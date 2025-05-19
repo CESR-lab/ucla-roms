a0d229aa865eeb82312fafe678a22aed16b2c3c7
Jeroen Molemaker <nmolem@atmos.ucla.edu>
2025-05-19 14:46:00 -0700

    Revert "Resolve complex conflict with ifx_functionality"
    This reverts commit e1991704bc470cefc0acf1b5a78ced10f5b9fa2c.

--- Examples/code_check/do_test_all.sh
+++ Examples/code_check/do_test_all.sh
@@ -2,29 +2,45 @@
 
 # ******** USER INPUT START ************
 # declare an array of Example folders and .in names to use:
-#declare -a Examples=( "Flux_frc" "Pipes_ana" "Pipes_real" "Rivers_ana" "Rivers_real" "Tracers_passive" "WEC_real" )
-declare -a Examples=( "Flux_frc" "Pipes_ana" "Pipes_real" "Rivers_ana" "Rivers_real" "Filament" )
+declare -a Examples=( "Flux_frc" "Pipes_ana" "Pipes_real" "Rivers_ana" "Rivers_real" "Filament" "bgc_real" )
 # ******** USER INPUT END   ************
 
 arg=$1
 
-if [ "$arg" != "expanse" -a "$arg" != "maya" -a "$arg" != "laptop" ]
-then
-echo "Script must have argument 'expanse' or 'maya'! E.g.: './do_test_all.sh maya'. Try again!"
-exit
-fi
+case "$arg" in
+    expanse|maya|laptop|github|github_ifx)
+	echo "running test for $arg"
+	;;
+    *)
+	echo "Script must have argument 'expanse' or 'maya'! E.g.: './do_test_all.sh maya'. Try again!"
+	exit
+	;;
+esac
                                                  
 error_cnt=0                                      # count of exit codes from each test
 total=${#Examples[*]}                            # total number of examples
 for (( i=0; i<=$(( $total -1 )); i++ ))          # run test cases:
 do
-  cd ../${Examples[i]}/code_check/
-  echo "${Examples[i]}:"
-  ./do_test_roms.sh $arg
-  
-  retval=$?                                      # $? gives exit code from ./do_test_roms.sh
-  error_cnt=$(( $error_cnt + $retval ))          
-  if [ $error_cnt -gt 0 ]
+    cd ../${Examples[i]}/code_check/
+    echo "##############################"
+    echo "${Examples[i]}:"
+    echo "##############################"
+
+    case ${Examples[i]} in
+	bgc_real)
+	    ./do_test_roms.sh $arg BEC
+	    retval=$?
+	    error_cnt=$(( $error_cnt + $retval ))
+	    ./do_test_roms.sh $arg MARBL
+	    ;;
+	*)
+	    ./do_test_roms.sh $arg
+	    ;;
+    esac
+    retval=$?                                      # $? gives exit code from ./do_test_roms.sh
+    error_cnt=$(( $error_cnt + $retval ))
+    
+  if [ $retval -ne 0 ]
   then
     echo -e "  test failed! \n"
 #   break
@@ -35,9 +51,11 @@ done
 
 if [ $error_cnt -eq 0 ]
 then
-  echo "ALL TESTS SUCCESSFUL!"
+    echo "ALL TESTS SUCCESSFUL!"
+    exit 0
 else
-  echo "ERROR - A TEST FAILED!"
+    echo "ERROR - A TEST FAILED!"
+    exit 1
 fi
 
 # Notes:
