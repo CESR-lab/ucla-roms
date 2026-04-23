dc588461a84713777f5ab7a307406c5a85932d4d
Jeroen Molemaker <nmolem@atmos.ucla.edu>
2026-04-23 07:50:43 -0700

    Detiling of set_forces.F
    Include the downstream code like bulk force and the analytical forcing.
    
    IMPORTANT: replaced the various ana surface forcing cpp keys with a single:
      ANA_SURFLUX key

--- src/ana_tide.h
+++ src/ana_tide.h
@@ -20,14 +20,14 @@
       kh  = ftide(1)/c
 
       if (bry_tides) then
-       do j=-1,ny+2          ! Extended ranges for x,y arrays
-        do i=-1,nx+2
+       do j=1-bf,ny+bf         ! Extended ranges for x,y arrays
+        do i=1-bf,nx+bf
 	  ztide_r(i,j,1) = amp*real(exp(-yr(i,j)/Ld)*exp(cI*kh*xr(i,j)))
 	  ztide_i(i,j,1) =-amp*aimag(exp(-yr(i,j)/Ld)*exp(cI*kh*xr(i,j)))
         enddo
        enddo
-       do j= 0,ny+1          ! Extended ranges for x,y arrays
-        do i= 0,nx+1
+       do j=1-bf,ny+bf        ! Extended ranges for x,y arrays
+        do i=1-bf,nx+bf
 	  utide_r(i,j,1) = 0.5*(ztide_r(i,j,1)+ztide_r(i-1,j,1))*g/c
 	  utide_i(i,j,1) = 0.5*(ztide_i(i,j,1)+ztide_i(i-1,j,1))*g/c
 	  vtide_r(i,j,1) = 0.
@@ -37,8 +37,8 @@
       endif
       kh = 2*pi/(2e6*1.4)
       if (pot_tides) then
-       do j=-1,ny+2          ! Extended ranges for x,y arrays
-        do i=-1,nx+2
+       do j=1-bf,ny+bf        ! Extended ranges for x,y arrays
+        do i=1-bf,nx+bf
 	  ptide_r(i,j,1) = amp*real(exp(cI*kh*xr(i,j)))
 	  ptide_i(i,j,1) =-amp*aimag(exp(cI*kh*xr(i,j)))
         enddo
